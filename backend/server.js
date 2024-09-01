faa8197720ab879ca5539548fc413fa002805431
sachan13harshit <harshit.23bcs10134@sst.scaler.com>
2024-09-01 16:15:48 +0530

    Updated the register page frontend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+// const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+connectDB();
+
+
+// Middleware
+// app.use(cors());
+app.use(express.json());
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
